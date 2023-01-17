a851f329c5639e001647b0ae7e73a1eb4f07d4b5
shikhu51197 <sg780060@gmail.com>
2023-01-17 15:53:40 +0530

    start your work

---
+++ myproject/src/Pages/MainRoute.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Routes , Route} from "react-router-dom"
+const MainRoute = () => {
+  return (
+    <div>
+    <Routes>
+        <Route/>
+        <Route/>
+        <Route/>
+    </Routes>
+      
+    </div>
+  )
+}
+
+export default MainRoute
