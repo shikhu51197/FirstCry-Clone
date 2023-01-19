b005cbbef0895d673840e6fc626474200439ab78
RoshaniChouhan <101568343+RoshaniChouhan@users.noreply.github.com>
2023-01-20 00:27:57 +0530

    carsoules

--- myproject/src/App.js
+++ myproject/src/App.js
@@ -4,12 +4,14 @@ import FristNav from './Navbar/FristNav';
 import Catbar from './Navbar/Catbar';
 import Cart from './Components/Cart';
 import Login from './Registration/Login';
+import Carousels from './Carousels/Carousels';
 function App() {
   return (
     <div className="App">
     <h1> My App</h1>
        <FristNav/>
     <Catbar/>
+    <Carousels/>
       <Login />
   <Cart/>
   </div>
