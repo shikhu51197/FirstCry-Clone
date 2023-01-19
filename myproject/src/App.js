806c83c22adfa9bd1e350e2c78c54c83b2ad50e6
UMARJAAN <umerbinfarooq123@gmail.com>
2023-01-19 15:12:55 +0530

    0394 day2

--- myproject/src/App.js
+++ myproject/src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
-
+import Login from './Registration/Login';
 function App() {
   return (
     <div className="App">
       <h1>First Cry</h1>
+      <Login />
     </div>
   );
 }
