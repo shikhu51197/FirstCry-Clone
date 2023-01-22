76c00f5b64c9ccc44d89e86fd009dc1bdb96e4ed
rohit715131 <rohit715131@gmail.com>
2023-01-22 23:42:18 +0530

    changes in products

--- myproject/src/Product/SingleProduct.jsx
+++ myproject/src/Product/SingleProduct.jsx
@@ -286,7 +286,7 @@ const SingleProduct = () => {
                 </Box>
               )}
               {flag == true && (
-                <Link>
+                <Link to="/cart">
                   <Box
                     h="50px"
                     w="150%"
@@ -297,7 +297,7 @@ const SingleProduct = () => {
                     cursor="pointer"
                   >
                     <Text fontSize="20px" mt="10px" color="white">
-                      Go TO CART
+                      GO TO CART
                     </Text>
                   </Box>
                 </Link>
