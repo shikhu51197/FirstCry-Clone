792b3aaf5c1fe2f58ba620e3397fb6fc6c35b9c4
rohit715131 <rohit715131@gmail.com>
2023-01-22 18:57:19 +0530

    changes in my account

---
+++ myproject/src/MyAccountPages/MyAccountPages.jsx
@@ -0,0 +1,17 @@
+import MyAccount from "../MyAccount/MyAccount";
+import Catbar from "../Navbar/Catbar";
+import FristNav from "../Navbar/FristNav";
+import Footer from "../Footer/Footer";
+
+const MyAccountPages = () => {
+  return (
+    <>
+      <FristNav />
+      <Catbar />
+      <MyAccount />
+      <Footer />
+    </>
+  );
+};
+
+export default MyAccountPages;
--- myproject/src/Pages/MainRoute.jsx
+++ myproject/src/Pages/MainRoute.jsx
@@ -1,33 +1,32 @@
-import React from 'react'
-import {Routes , Route} from "react-router-dom"
-import Admin from '../AdminPanel/Admin'
-import Adminlogin from '../AdminPanel/Adminlogin '
-import Cart from '../Components/Cart'
-import Home from '../HomePage/Home'
-import MyAccount from '../MyAccount/MyAccount'
-import Products from '../Product/Products'
-import SingleProduct from '../Product/SingleProduct'
-import Login from '../Registration/Login'
-import Signup from '../Registration/Signup'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Admin from "../AdminPanel/Admin";
+import Adminlogin from "../AdminPanel/Adminlogin ";
+import Cart from "../Components/Cart";
+import Home from "../HomePage/Home";
+import MyAccountPages from "../MyAccountPages/MyAccountPages";
+import Products from "../Product/Products";
+import SingleProduct from "../Product/SingleProduct";
+import Login from "../Registration/Login";
+import Signup from "../Registration/Signup";
 const MainRoute = () => {
   return (
     <div>
-    <Routes>
-    <Route path="/" element={<Home/>} />  
-    <Route path="/cart" element={<Cart/>} />
-    <Route path="/login" element={<Login/>} />
-    <Route path="/signup" element={<Signup/>} />
-    <Route path="/singleproduct/:id" element={<SingleProduct />} />
-    <Route path="/account" element={<MyAccount/>} />
-    <Route path="/product" element={<Products />} />
-    <Route path="/adminlogin" element={<Adminlogin/>} />
-    <Route path="/admin" element={<Admin/>} />
-        <Route/>
-        <Route/>
-    </Routes>
-      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/singleproduct/:id" element={<SingleProduct />} />
+        <Route path="/account" element={<MyAccountPages />} />
+        <Route path="/product" element={<Products />} />
+        <Route path="/adminlogin" element={<Adminlogin />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route />
+        <Route />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default MainRoute
+export default MainRoute;
